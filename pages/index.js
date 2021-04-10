dd3beda49cc5edbb921e09cae6a63d0fa65b9fe7
João Portela <jportela@abyssal.eu>
2021-04-10 15:42:11 +0100

    Site skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,44 @@
 import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
+
+import Cv1 from '@components/Cv1'
+import Cv2 from '@components/Cv2'
+import Cv3 from '@components/Cv3'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>João Portela</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1 className="title">João Portela</h1>
+        <p>Software Engineer &nbsp;&bull;&nbsp; 10+ years of experience &nbsp;&bull;&nbsp; Likes to be
+					challenged</p>
+        <nav>
+          <ul>
+            <li><a href="https://github.com/joaoportela/" className="icon"><FontAwesomeIcon icon={faGithub} /><span className="label">Github</span></a></li>
+            <li><a href="https://www.linkedin.com/in/joao-santos-portela" className="icon"><FontAwesomeIcon icon={faLinkedin} /><span className="label">LinkedIn</span></a></li>
+            <li><a href="cv_joao_portela.pdf" className="icon"><FontAwesomeIcon icon={faFilePdf} /><span className="label">CV</span></a></li>
+            <li><a href="cv_joao_portela.pdf" className="icon"><Cv1 /><span className="label">CV</span></a></li>
+            <li><a href="cv_joao_portela.pdf" className="icon"><Cv2 /><span className="label">CV</span></a></li>
+            <li><a href="cv_joao_portela.pdf" className="icon"><Cv3 /><span className="label">CV</span></a></li>
+          </ul>
+        </nav>
       </main>
 
-      <Footer />
+      <footer>
+        <span>Design inspired by Aerial from <a href="http://html5up.net">HTML5 UP</a>.</span>
+      </footer>
     </div>
   )
 }
