73149e770af546eedc397f2eeb3c6dc59b75e7bc
João Paulo dos Santos Portela <joaoportela@users.noreply.github.com>
2024-06-26 20:39:45 +0000

    Somewhat functional update script

---
+++ .scripts/update_cv.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -eu
+
+FILE_TO_UPDATE="public/cv_joao_portela.pdf"
+GDRIVE_OUT_FILE="gdrive_cv_joao_portela.pdf"
+
+# wget ${CV_DROPBOX_DOWNLOAD_URL} -O "dropbox_cv_joao_portela.pdf"
+
+wget ${CV_GDRIVE_DOWNLOAD_URL} -O "${GDRIVE_OUT_FILE}"
+
+# check if file is valid
+file "${GDRIVE_OUT_FILE}" | grep PDF || (echo "Invalid file type: '$(file "${GDRIVE_OUT_FILE}")'" && exit 1)
+
+mv "${GDRIVE_OUT_FILE}" "${FILE_TO_UPDATE}"
+
+# git add "${FILE_TO_UPDATE}"
+# git commit -m "Updated cv_joao_portela.pdf" && git push
